94631c12baaa972ef321d0a4d9e4d04f1b68f213
oboro <my.ancestor.skanderbeg@gmail.com>
2024-09-17 22:30:09 +0900

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,23 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   experimental: {
+//     esmExternals: 'loose'
+//   },
+//   webpack: (config) => {
+//     config.externals = [...config.externals, { canvas: "canvas" }]; // required to make pdfjs work
+//     return config;
+//   },
+// };
+
+// module.exports = nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/CyberPowerIndex',
+  assetPrefix: '/CyberPowerIndex/',
   experimental: {
-    esmExternals: 'loose'
+    esmExternals: 'loose',
   },
   webpack: (config) => {
     config.externals = [...config.externals, { canvas: "canvas" }]; // required to make pdfjs work
